74deec22c5cb270b5e5a288da96d4a4139a4522a
Yuji ODA <yuji.od@gmail.com>
2019-10-04 21:22:08 +0900

    Added Laravel Telescope.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Telescope\TelescopeServiceProvider;
+use Laravel\Telescope\Telescpoe;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Add Telescope only local development
+        // if ($this->app->isLocal()) {
+        //     $this->app->register(TelescopeServiceProvider::class);
+        // } else {
+        //     Telescope::ignoreMigrations();
+        // }
     }
 
     /**
